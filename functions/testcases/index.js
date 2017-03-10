f674c83c6146ed466bd31be7ede7a2b87e1f70b0
Sami Hostikka <sami.hostikka@sysart.fi>
2017-03-10 17:52:07 +0200

    Refactor fns

--- functions/testcases/index.js
+++ functions/testcases/index.js
@@ -6,6 +6,10 @@ const Joi = require('joi');
 const schema = require('./schema');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const headers = {
+  "Access-Control-Allow-Origin" : "*",
+  "Access-Control-Allow-Credentials" : true
+};
 
 module.exports.create = (event, context, callback) => {
   const timestamp = new Date().getTime();
@@ -40,15 +44,65 @@ module.exports.create = (event, context, callback) => {
 
     const response = {
       statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin" : "*",
-        "Access-Control-Allow-Credentials" : true
-      },
+      headers
       body: JSON.stringify({
         id: item.id
       }),
     };
 
     callback(null, response);
+  });
+};
+
+module.exports.get = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch the todo item.'));
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(result.Item),
+    };
+
+    callback(null, response);
+  });
+};
+
+module.exports.list = (event, ctx, cb) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    AttributesToGet: [
+      'id',
+      'title',
+      'createdAt',
+      'description'
+    ]
+  };
+
+  dynamoDb.scan(params, (err, data) => {
+    if(err) {
+      console.error(err);
+      cb(new Error('An error occured'));
+      return;
+    }
+
+    const res = {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(data)
+    };
+
+    cb(null, res);
   });
 };
