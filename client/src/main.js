9d1a9d4faec9591a82e055bab08a5e7f4bdb0a93
Joni Mertoniemi <joni.mertoniemi@sysart.fi>
2017-04-13 13:16:17 +0300

    change materialize

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,5 @@
-import './materialize.scss'
+require('materialize-css/dist/js/materialize.js')
+require('materialize-css/dist/css/materialize.min.css')
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
